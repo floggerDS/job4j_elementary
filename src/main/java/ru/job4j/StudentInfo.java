3f7b06c5d1eb8b79015fd831a15f0371703a240b
Denis_PC <den.mitskevich@gmail..com>
2022-11-10 17:36:29 +0300

    4.1. Разбор первой программы. [#159033 #126530]

--- src/main/java/ru/job4j/StudentInfo.java
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -2,7 +2,7 @@ package ru.job4j;
 
 public class StudentInfo {
     public static void main(String[] args) {
-        System.out.println("Denis  Mitskevich");
+        System.out.println("Denis Mitskevich");
         System.out.println("12.09.1988");
     }
 }
