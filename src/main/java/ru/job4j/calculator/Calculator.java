2439bf964c4426599d8be3f45bf7dfb723fbb174
Den_Note <den.mitskevich@gmail.com>
2024-01-26 08:36:06 +0300

    4.2. Результат работы метода [#232622 #126516]

--- src/main/java/ru/job4j/calculator/Calculator.java
+++ src/main/java/ru/job4j/calculator/Calculator.java
@@ -1,12 +1,13 @@
 package ru.job4j.calculator;
 
 public class Calculator {
-    public static int plus(int first, int second) {
-        return first + second;
+    public static void plus(int first, int second) {
+        int result = first + second;
+        System.out.println(result);
     }
 
     public static void main(String[] args) {
-        int result3 = plus(100, 100);
-        System.out.println(result3);
+        Calculator.plus(1, 2);
+        Calculator.plus(10, 11);
     }
 }
