41a5c5e06f5732a49f93ce05659477f224730b36
Den_Note <den.mitskevich@gmail.com>
2024-01-26 10:03:57 +0300

    8. Стороны прямоугольника. [#137735 #126506]

---
+++ src/main/java/ru/job4j/condition/RectangleArea.java
@@ -0,0 +1,19 @@
+package ru.job4j.condition;
+
+/**
+ * В этом задании необходимо вычислить площадь прямоугольника. Известны периметр и соотношение сторон прямоугольника.
+ */
+public class RectangleArea {
+    public static double square(int p, int k) {
+        double h = p / (2 * (k + 1));
+        double l  = h * k;
+        return l * h;
+    }
+
+    public static void main(String[] args) {
+        double result1 = RectangleArea.square(4, 1);
+        System.out.println(" p = 4, k = 1, s = 1, real = " + result1);
+        result1 = RectangleArea.square(6, 2);
+        System.out.println(" p = 6, k = 2, s = 2, real = " + result1);
+    }
+}
