3d6b4bc6c01688cd939485eb9f394be1ace9470e
Den <den.mitskevich@gmail.com>
2024-02-05 20:37:35 +0300

    4. Тесты для стороны прямоугольника. [#159460 #126542]

--- src/main/java/ru/job4j/condition/RectangleArea.java
+++ src/main/java/ru/job4j/condition/RectangleArea.java
@@ -4,7 +4,7 @@ package ru.job4j.condition;
  * В этом задании необходимо вычислить площадь прямоугольника. Известны периметр и соотношение сторон прямоугольника.
  */
 public class RectangleArea {
-    public static double square(int p, int k) {
+    public static double square(int p, double k) {
         double h = p / (2 * (k + 1));
         double l  = h * k;
         return l * h;
@@ -15,5 +15,7 @@ public class RectangleArea {
         System.out.println(" p = 4, k = 1, s = 1, real = " + result1);
         result1 = RectangleArea.square(6, 2);
         System.out.println(" p = 6, k = 2, s = 2, real = " + result1);
+        result1 = RectangleArea.square(19, 7);
+        System.out.println(" p = 19, k = 7, s = 9.81, real = " + result1);
     }
 }
---
+++ src/test/java/ru/job4j/condition/RectangleAreaTest.java
@@ -0,0 +1,36 @@
+package ru.job4j.condition;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+class RectangleAreaTest {
+
+    @Test
+    void whenP6K2Square2() {
+        int expected = 2;
+        int p = 6;
+        double k = 2;
+        double output = RectangleArea.square(p, k);
+        assertThat(output).isEqualTo(expected, withPrecision(0.01));
+    }
+
+    @Test
+    void whenP8K3Square2() {
+        int expected = 3;
+        int p = 8;
+        double k = 3;
+        double output = RectangleArea.square(p, k);
+        assertThat(output).isEqualTo(expected, withPrecision(0.01));
+    }
+
+    @Test
+    void whenP19K7Square9Dot87() {
+        double expected = 9.87;
+        int p = 19;
+        double k = 7;
+        double output = RectangleArea.square(p, k);
+        assertThat(output).isEqualTo(expected, withPrecision(0.01));
+    }
+
+}
